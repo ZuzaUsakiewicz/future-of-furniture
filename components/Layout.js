1678771f77c3fe5d608233e9114bde2543bee26f
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-09 15:45:22 +0200

    create layout component

--- components/Layout.js
+++ components/Layout.js
@@ -6,11 +6,47 @@ const GlobalStyle = createGlobalStyle`
   padding: 0;
   margin: 0;
 }
- h1 {
+body{
+  font-family: Kanit;
+  font-weight: 300;
+}
+h1 {
    font-size: 4rem;
    color: pink;
-   text-transform: uppercase;
- }
+   font-family: Satoshi;
+    }
+.swiper-container {
+      width: 100%;
+      height: 80vh;
+    }
+.swiper-slide {
+      cursor: grab;
+      text-align: center;
+      font-size: 18px;
+      background: #fff;
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      -webkit-justify-content: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      align-items: center;
+      &:active {
+        cursor: grabbing;
+      }
+    }
+.swiper-button-prev,.swiper-button-next{
+    display: none;
+    pointer-events: none;
+    }
+.swiper-pagination-bullet{
+  background-color: black;
+    }
 `;
 const Container = styled.div`
   text-align: center;
