0c73fd5a4891b0029cb9f098873efd028d20982d
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-09 15:47:42 +0200

    add fonts and swiper components links

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -27,4 +27,48 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;1,300&family=Prata&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://api.fontshare.com/css?f[]=satoshi@300&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@6.5.5/components/navigation/navigation.min.css"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@6.5.5/components/scrollbar/scrollbar.min.css"
+          ></link>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@6.5.5/components/pagination/pagination.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/swiper@6.5.5/swiper.min.css"
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+{
+  /* <link
+            href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+            rel="stylesheet"
+          /> */
 }
