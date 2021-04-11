020c403d75b432b59b71b4af4e4b67e5e1533b62
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-11 16:21:36 +0200

    modified navbar component

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,33 +2,42 @@ import React from "react";
 import { NavbarContainer, NavItem } from "../../styles/Navbar.styled";
 import HamburgerMenu from "./HamburgerMenu";
 import Image from "next/image";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <NavbarContainer>
-      <NavItem>
-        <Image
-          src="/favicon.ico"
-          alt="Futuristic logo"
-          layout="fill"
-          objectFit="contain"
-          quality={100}
-          priority={true}
-          objectPosition="center"
-        />
-      </NavItem>
+      <Link href="/about">
+        <a>
+          <NavItem left>
+            <Image
+              src="/icon.svg"
+              alt="contact icon"
+              layout="fill"
+              objectFit="contain"
+              quality={100}
+              priority={true}
+              objectPosition="center"
+            />
+          </NavItem>
+        </a>
+      </Link>
       <HamburgerMenu />
-      <NavItem>
-        <Image
-          src="/futureoff.png"
-          alt="Futuristic logo"
-          layout="fill"
-          objectFit="contain"
-          quality={100}
-          priority={true}
-          objectPosition="center"
-        />
-      </NavItem>
+      <Link href="/">
+        <a>
+          <NavItem>
+            <Image
+              src="/futureoffwhite.svg"
+              alt="Futuristic logo"
+              layout="fill"
+              objectFit="contain"
+              quality={100}
+              priority={true}
+              objectPosition="center"
+            />
+          </NavItem>
+        </a>
+      </Link>
     </NavbarContainer>
   );
 }
