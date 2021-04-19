52c5c898c7dd7b847d10979bbff64481fc35d7ee
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-19 16:56:08 +0200

    moved GlobalStyle from Layout to app and fixed not formatting problem in createGlobalStyle component

--- components/Layout.js
+++ components/Layout.js
@@ -1,65 +1,10 @@
-import styled, { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-*, html {
-  box-sizing:border-box;
-  padding: 0;
-  margin: 0;
-}
-body{
-  font-family: Kanit;
-  font-weight: 300;
-}
-h1 {
-   font-size: 4rem;
-   color: pink;
-   font-family: Satoshi;
-    }
-.swiper-container {
-      width: 100%;
-      height: 80vh;
-    }
-.swiper-slide {
-      cursor: grab;
-      text-align: center;
-      font-size: 18px;
-      background: #fff;
-      display: -webkit-box;
-      display: -ms-flexbox;
-      display: -webkit-flex;
-      display: flex;
-      -webkit-box-pack: center;
-      -ms-flex-pack: center;
-      -webkit-justify-content: center;
-      justify-content: center;
-      -webkit-box-align: center;
-      -ms-flex-align: center;
-      -webkit-align-items: center;
-      align-items: center;
-      &:active {
-        cursor: grabbing;
-      }
-    }
-.swiper-button-prev,.swiper-button-next{
-    display: none;
-    pointer-events: none;
-    }
-.swiper-pagination-bullet{
-  background-color: black;
-    }
-`;
-const Container = styled.div`
-  text-align: center;
-`;
+import Navbar from "../components/Navbar/Navbar";
 
 export default function Layout({ children }) {
   return (
-    <Container>
-      <GlobalStyle />
-      <h1>hello</h1>
-      {/* <Navbar/> */}
+    <>
+      <Navbar />
       {children}
-      {/* <Footer/> */}
-    </Container>
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,99 @@
 import Layout from "../components/Layout";
-import "../styles/globals.css";
+import "swiper/swiper.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/scrollbar/scrollbar.min.css";
+import Theme from "../components/Theme";
+import * as styled from "styled-components";
+
+const GlobalStyle = styled.createGlobalStyle`
+  *,
+  html {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    font-family: Kanit;
+    font-weight: 400;
+    font-size: 20px;
+    min-height: 200vh;
+    background: ${({ theme }) => theme.colors.primaryDark};
+    color: ${({ theme }) => theme.colors.primaryLight};
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  a {
+    z-index: 10;
+  }
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.primaryLight};
+  }
+  h1 {
+    font-size: ${({ theme }) => theme.fontSize.large};
+    font-family: Satoshi;
+    z-index: 2;
+    position: relative;
+  }
+  button {
+    cursor: pointer;
+    background: transparent;
+    border: none;
+    outline: none;
+    font-family: Satoshi;
+    &:focus-visible {
+      outline: 2px solid black;
+    }
+  }
+  .swiper-container {
+    width: 100%;
+    height: 80vh;
+  }
+  .swiper-slide {
+    cursor: grab;
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+    &:active {
+      cursor: grabbing;
+    }
+  }
+  .swiper-button-prev,
+  .swiper-button-next {
+    display: none;
+    pointer-events: none;
+  }
+  .swiper-pagination-bullet {
+    background-color: black;
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Theme>
+      <GlobalStyle />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Theme>
   );
 }
 
