8f5cfe2519620dcd1aea88a82121b05c063602bf
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-04-20 21:08:02 +0200

    created FlexContainer component for flex and center everything in it

---
+++ components/FlexContainer.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FlexContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  text-align: center;
+  position: relative;
+`;
+
+export default FlexContainer;
