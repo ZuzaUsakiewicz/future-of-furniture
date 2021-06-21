1688c54a8887a6b5bc179a28f68363ef7a34b7f1
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-06-21 12:14:06 +0200

    update tiles container

--- components/HomePage/TilesContainer.js
+++ components/HomePage/TilesContainer.js
@@ -2,31 +2,35 @@ import React from "react";
 import FlexContainer from "../FlexContainer";
 import Image from "next/image";
 import Link from "next/link";
-import { TileContainer, TileCard, Overlay } from "../../styles/HomePage.styled";
+import {
+  TileContainer,
+  TileCard,
+  TileContainerTitle,
+} from "../../styles/HomePage.styled";
 
 const tiles = [
   {
-    name: "first tile",
+    name: "kitchens",
     category: "kitchen",
+    img: "/kitchen.jpg",
+    link: "/kitchen",
+  },
+  {
+    name: "daily",
+    category: "living room",
+    img: "/sofa2.jpg",
+    link: "/livingroom",
+  },
+  {
+    name: "dinner time",
+    category: "dining room",
     img: "/kitchen2.jpg",
     link: "/kitchen",
   },
   {
-    name: "second tile",
-    category: "living room",
-    img: "/sofa.jpg",
-    link: "/livingroom",
-  },
-  {
-    name: "third tile",
-    category: "dining room",
-    img: "/chair2.jpg",
-    link: "/kitchen",
-  },
-  {
-    name: "fourth tile",
+    name: "the office",
     category: "office",
-    img: "/kitchen.jpg",
+    img: "/chair2.jpg",
     link: "/livingroom",
   },
 ];
@@ -34,17 +38,18 @@ const tiles = [
 function TilesContainer() {
   return (
     <FlexContainer>
+      <TileContainerTitle>2021 Trends</TileContainerTitle>
       <TileContainer>
         {tiles.map((tile) => (
           <TileCard key={tile.name}>
-            <Link href="/">
+            <Link href={tile.link}>
               <a>
                 <h2> {tile.name}</h2>
                 <Image
                   src={tile.img}
                   alt={tile.name}
                   layout="intrinsic"
-                  width={300}
+                  width={280}
                   height={300}
                   objectFit="cover"
                 />
